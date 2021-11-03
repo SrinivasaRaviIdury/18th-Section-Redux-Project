974339d1cc68771650d2323a1f51ef383a0de7ab
Srinivasa-Ravi-Idury <42990095+SrinivasaRaviIdury@users.noreply.github.com>
2021-11-03 13:35:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello I'm Redux</h1>
+      <h2>State Management Library</h2>
+    </div>
+  );
+}
